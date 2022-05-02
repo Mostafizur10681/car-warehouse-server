e29ca299f86d64a62905a71d88a4753a373593fe
Mostafizur rahman <sourav10681@gmail.com>
2022-05-02 15:31:28 +0600

    package installation and mongodb connected

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.PASSWORD}@cluster0.plbgo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    console.log('Genius car db connected')
+    // perform actions on the collection object
+    client.close();
+});
+
+app.get('/', (req, res) => {
+    res.send('Running Car Warehouse Server')
+})
+
+app.listen(port, (req, res) => {
+    console.log('Listening to port', port)
+})
