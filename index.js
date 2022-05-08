6887fb0c7145c87a04aa52974d1b7883d8618e14
Mostafizur rahman <sourav10681@gmail.com>
2022-05-08 18:53:12 +0600

    add item & login api created

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
@@ -9,7 +10,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: "unauthorized access" })
+    }
+    next();
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.PASSWORD}@cluster0.plbgo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -73,6 +80,24 @@ async function run() {
                 $set: { quantity: newQuantity }
             })
             res.send(updateQuantity);
+        })
+
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken });
+        })
+
+        // add item
+        app.get('/myInventory', verifyJWT, async (req, res) => {
+            const email = req.query.email;
+            console.log(email)
+            const query = { email: email }
+            const cursor = inventoryCollecttion.find(query);
+            const items = await cursor.toArray();
+            res.send(items)
         })
     }
     finally {
